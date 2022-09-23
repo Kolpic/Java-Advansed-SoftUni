22e35e7b36fb27a76c3d3956e8bb9e8efb9ff420
Kolpic <galincho112@gmail.com>
2022-09-24 00:14:28 +0300

    Added new task

---
+++ Exercise_Stacks_and_Queues/Simple_Text_Editor_07.java
@@ -0,0 +1,12 @@
+package Exercise_Stacks_and_Queues;
+
+import java.util.Scanner;
+
+public class Simple_Text_Editor_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = null;
+        int numberOfOperations = Integer.parseInt(scanner.nextLine());
+    }
+}
