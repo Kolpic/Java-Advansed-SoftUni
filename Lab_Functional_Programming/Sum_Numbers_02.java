6156d5f15927c7f0cdf5dbb80305111e158cb807
Kolpic <galincho112@gmail.com>
2022-10-24 19:27:41 +0300

    Added new task

---
+++ Lab_Functional_Programming/Sum_Numbers_02.java
@@ -0,0 +1,23 @@
+package Lab_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Sum_Numbers_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Function<int[],String> count = elements -> "Count = " +
+                Arrays.stream(elements).count();
+
+        Function<int[],String> sum = elements -> "Sum = " +
+                Arrays.stream(elements).sum();
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(count.apply(numbers));
+        System.out.println(sum.apply(numbers));
+    }
+}
