051973889aae8a14877f3f397747ee8e52be9441
Kolpic <galincho112@gmail.com>
2022-11-10 19:39:48 +0200

    Added new task

---
+++ Exercises_Generics/Generic Count Method Strings_05/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<String> box = new Box<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            box.add(input);
+        }
+
+        String command = scanner.nextLine();
+
+
+        System.out.println(box.compareTo(command));
+    }
+}
