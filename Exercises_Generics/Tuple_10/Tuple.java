6efe2293ac9e2c52b5ba530f45bd7b4ca0523d82
Kolpic <galincho112@gmail.com>
2022-11-13 10:55:24 +0200

    Added new task

---
+++ Exercises_Generics/Tuple_10/Tuple.java
@@ -0,0 +1,14 @@
+public class Tuple<F,S> {
+    private F first;
+    private S second;
+
+    public Tuple(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -> %s",first.toString(),second.toString());
+    }
+}
